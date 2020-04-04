c6200712ca5639d325609a61fc6c7c56a54fcf2d
Zaman <Sunny.Zaman@ontario.ca>
2020-04-04 19:11:07 -0400

    Created admin mode

--- views/header.php
+++ views/header.php
@@ -4,23 +4,25 @@ session_start();
 ?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">
-        <img src="favicon.png" width="30" height="30" class="d-inline-block align-top" alt="">
-        RU Travel
+        <img src="favicon.png" width="30" height="35" class="d-inline-block align-top" alt="">
+        <span class="underline">RU Travel</span>
       </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse justify-content-end" id="navbarText">
       <ul class="navbar-nav">
+      <?php if($_SESSION['isAdmin']==='0') : ?>
         <li class="nav-item active">
           <a class="nav-link" href="#">Home</a>
         </li>
-        <li class="nav-item" ng-if="!admin">
+        <li class="nav-item">
           <a class="nav-link" href="#">Features</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Pricing</a>
         </li>
+        <?php endif; ?>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <?php echo $_SESSION["FirstName"]. ' ' .$_SESSION["LastName"];?>
