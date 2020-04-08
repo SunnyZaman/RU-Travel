9ca9f516895753b630dce7c9ec78c3a78e640c85
Zaman <Sunny.Zaman@ontario.ca>
2020-04-08 11:14:02 -0400

    Created Reviews Table

--- server/database.php
+++ server/database.php
@@ -34,6 +34,7 @@
         AttractionDescription VARCHAR(250) NOT NULL,
         Price FLOAT NOT NULL,
         Coordinates VARCHAR(250) NOT NULL,
+        RatingTotal INT DEFAULT 0,
         Attraction VARCHAR(250) NOT NULL,
         FOREIGN KEY (Attraction)
         REFERENCES RUTravelPlaces(Attraction)
@@ -41,6 +42,23 @@
         ON UPDATE CASCADE
         )";
     //    SELECT * FROM RUTravelPlaces INNER JOIN RUTravelAttractions USING (Attraction) WHERE Country ='India';
+    if ($conn->query($sql) === FALSE){
+        echo "Error creating table: " . $conn->error;
+    }
+    $sql = "CREATE TABLE IF NOT EXISTS RUTravelReviews(
+        Id INT PRIMARY KEY AUTO_INCREMENT,
+        ReviewerName VARCHAR(250) NOT NULL,
+        ReviewerEmail VARCHAR(250) NOT NULL,
+        ReviewDescription VARCHAR(250) NOT NULL,
+        ReviewTitle FLOAT NOT NULL,
+        ReviewDate VARCHAR(250) NOT NULL,
+        ReviewRating INT NOT NULL,
+        Attraction VARCHAR(250) NOT NULL,
+        FOREIGN KEY (Attraction)
+        REFERENCES RUTravelAttractions(Attraction)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+        )";
     if ($conn->query($sql) === FALSE){
         echo "Error creating table: " . $conn->error;
     }
@@ -102,22 +120,22 @@
     //       VALUES ('GroberArber.jpg', '94252 Bayerisch Eisenstein, Germany', 'Mountain offering picturesque views, a forest park, hiking trails & winding roads for motorbiking.', 53.00, '{latitude: 49.112776, longitude: 13.118046}', 'Grober Arber');";
     //       $sql .= "INSERT INTO RUTravelAttractions(AttractionImage, AttractionAddress, AttractionDescription, Price, Coordinates, Attraction)
     //       VALUES ('RoyalOntarioMuseum.jpg', '100 Queens Park, Toronto, ON M5S 2C6', 'Sprawling natural history & world cultures galleries, plus dinosaurs in the Libeskind crystal wing.', 18.50, '{latitude: 43.6677097, longitude: -79.3947771}', 'Royal Ontario Museum');";
-    //        $sql .= "INSERT INTO RUTravelAttractions(AttractionImage, AttractionAddress, AttractionDescription, Price, Coordinates, Attraction)
-    //        VALUES ('StatueofLiberty.jpg', 'New York, NY 10004, United States', 'Iconic National Monument opened in 1886, offering guided tours, a museum & city views.', 13.00, '{latitude: 40.6892494, longitude: -74.0445004}', 'Statue of Liberty');";
-    //         $sql .= "INSERT INTO RUTravelAttractions(AttractionImage, AttractionAddress, AttractionDescription, Price, Coordinates, Attraction)
-    //         VALUES ('MuseumofModernArt.jpg', '11 W 53rd St, New York, NY 10019, United States', 'Works from van Gogh to Warhol & beyond plus a sculpture garden, 2 cafes & The Modern restaurant.', 65.00, '{latitude: 40.7614327, longitude: -73.9776216}', 'Museum of Modern Art');";
-    //          $sql .= "INSERT INTO RUTravelAttractions(AttractionImage, AttractionAddress, AttractionDescription, Price, Coordinates, Attraction)
-    //          VALUES ('GrandCanyonNationalPark.jpg', 'Grand Canyon Village, Arizona, United States', 'Beyond its scenic overlooks, this mile-deep geologic wonder features hikes, mule rides & rafting.', 30.00, '{latitude: 36.1069652, longitude: -112.1129972}', 'Grand Canyon National Park');";
-    //           $sql .= "INSERT INTO RUTravelAttractions(AttractionImage, AttractionAddress, AttractionDescription, Price, Coordinates, Attraction)
-    //           VALUES ('ChristtheRedeemer.jpg', 'Parque Nacional da Tijuca - Alto da Boa Vista, Rio de Janeiro - RJ, Brazil', 'Giant (98-ft.-tall) mountaintop statue of Jesus Christ, accessed by train & offering city views.', 24.00, '{latitude: -22.951916, longitude: -43.2104872}', 'Christ the Redeemer');";
-    //            $sql .= "INSERT INTO RUTravelAttractions(AttractionImage, AttractionAddress, AttractionDescription, Price, Coordinates, Attraction)
-    //            VALUES ('MachuPicchu.jpg', '08680, Peru', 'Iconic hilltop ruins of a large 15th-century Inca city featuring numerous structures & terraces.', 28.00, '{latitude: -13.1631412, longitude: -72.5449629}', 'Machu Picchu');";
-    //             $sql .= "INSERT INTO RUTravelAttractions(AttractionImage, AttractionAddress, AttractionDescription, Price, Coordinates, Attraction)
-    //             VALUES ('TuqilloBeach.jpg', '02650, Peru', 'Celebrated, peaceful beach featuring swimming, snorkeling, sunbathing, campsites & restaurants.', 11.00, '{latitude: -10.0202771, longitude: -78.206954}', 'Tuqillo Beach');";
-    //              $sql .= "INSERT INTO RUTravelAttractions(AttractionImage, AttractionAddress, AttractionDescription, Price, Coordinates, Attraction)
-    //              VALUES ('GreatBarrierReef.jpg', 'Queensland, Australia', 'The Great Barrier Reef is the largest coral reef system composed of over 2,900 individual reefs and 900 islands.', 115.00, '{latitude: -16.6084019, longitude: 143.5981648}', 'Great Barrier Reef');";
-    //                $sql .= "INSERT INTO RUTravelAttractions(AttractionImage, AttractionAddress, AttractionDescription, Price, Coordinates, Attraction)
-    //                VALUES ('SydneyOperaHouse.jpg', 'Bennelong Point, Sydney NSW 2000, Australia', 'Landmark, skyline-dominating arts centre for opera, theatre, music and dance, plus guided tours.', 30.00, '{latitude: -33.8567844, longitude: 151.2152967}', 'Sydney Opera House');";
+    //       $sql .= "INSERT INTO RUTravelAttractions(AttractionImage, AttractionAddress, AttractionDescription, Price, Coordinates, Attraction)
+    //      VALUES ('StatueofLiberty.jpg', 'New York, NY 10004, United States', 'Iconic National Monument opened in 1886, offering guided tours, a museum & city views.', 13.00, '{latitude: 40.6892494, longitude: -74.0445004}', 'Statue of Liberty');";
+    //      $sql .= "INSERT INTO RUTravelAttractions(AttractionImage, AttractionAddress, AttractionDescription, Price, Coordinates, Attraction)
+    //      VALUES ('MuseumofModernArt.jpg', '11 W 53rd St, New York, NY 10019, United States', 'Works from van Gogh to Warhol & beyond plus a sculpture garden, 2 cafes & The Modern restaurant.', 65.00, '{latitude: 40.7614327, longitude: -73.9776216}', 'Museum of Modern Art');";
+    //       $sql .= "INSERT INTO RUTravelAttractions(AttractionImage, AttractionAddress, AttractionDescription, Price, Coordinates, Attraction)
+    //      VALUES ('GrandCanyonNationalPark.jpg', 'Grand Canyon Village, Arizona, United States', 'Beyond its scenic overlooks, this mile-deep geologic wonder features hikes, mule rides & rafting.', 30.00, '{latitude: 36.1069652, longitude: -112.1129972}', 'Grand Canyon National Park');";
+    //      $sql .= "INSERT INTO RUTravelAttractions(AttractionImage, AttractionAddress, AttractionDescription, Price, Coordinates, Attraction)
+    //      VALUES ('ChristtheRedeemer.jpg', 'Parque Nacional da Tijuca - Alto da Boa Vista, Rio de Janeiro - RJ, Brazil', 'Giant (98-ft.-tall) mountaintop statue of Jesus Christ, accessed by train & offering city views.', 24.00, '{latitude: -22.951916, longitude: -43.2104872}', 'Christ the Redeemer');";
+    //      $sql .= "INSERT INTO RUTravelAttractions(AttractionImage, AttractionAddress, AttractionDescription, Price, Coordinates, Attraction)
+    //      VALUES ('MachuPicchu.jpg', '08680, Peru', 'Iconic hilltop ruins of a large 15th-century Inca city featuring numerous structures & terraces.', 28.00, '{latitude: -13.1631412, longitude: -72.5449629}', 'Machu Picchu');";
+    //       $sql .= "INSERT INTO RUTravelAttractions(AttractionImage, AttractionAddress, AttractionDescription, Price, Coordinates, Attraction)
+    //       VALUES ('TuqilloBeach.jpg', '02650, Peru', 'Celebrated, peaceful beach featuring swimming, snorkeling, sunbathing, campsites & restaurants.', 11.00, '{latitude: -10.0202771, longitude: -78.206954}', 'Tuqillo Beach');";
+    //       $sql .= "INSERT INTO RUTravelAttractions(AttractionImage, AttractionAddress, AttractionDescription, Price, Coordinates, Attraction)
+    //      VALUES ('GreatBarrierReef.jpg', 'Queensland, Australia', 'The Great Barrier Reef is the largest coral reef system composed of over 2,900 individual reefs and 900 islands.', 115.00, '{latitude: -16.6084019, longitude: 143.5981648}', 'Great Barrier Reef');";
+    //      $sql .= "INSERT INTO RUTravelAttractions(AttractionImage, AttractionAddress, AttractionDescription, Price, Coordinates, Attraction)
+    //      VALUES ('SydneyOperaHouse.jpg', 'Bennelong Point, Sydney NSW 2000, Australia', 'Landmark, skyline-dominating arts centre for opera, theatre, music and dance, plus guided tours.', 30.00, '{latitude: -33.8567844, longitude: 151.2152967}', 'Sydney Opera House');";
     // if ($conn->multi_query($sql) === TRUE) {
     //     echo "New records created successfully";
     // } else {
