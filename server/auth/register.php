c0ab66d39d241b6f203d89ffefb92d482de60889
Zaman <Sunny.Zaman@ontario.ca>
2020-04-11 04:02:41 -0400

    cleaned and updated

--- server/auth/register.php
+++ server/auth/register.php
@@ -5,31 +5,12 @@ $register_data = json_decode(file_get_contents("php://input"), true);
 $message = '';
 $registered = false;
 $isAdmin = 0;
-if(!empty($register_data['firstName']))
-{
  $firstName = $register_data['firstName'];
-}
-if(!empty($register_data['lastName']))
-{
  $lastName = $register_data['lastName'];
-}
-if(!empty($register_data['email']))
-{
   $email = $register_data['email'];
-}
-
-if(!empty($register_data['password']))
-{
  $password = password_hash($register_data['password'], PASSWORD_DEFAULT);
-}
-if(!empty($register_data['address']))
-{
  $address= $register_data['address'];
-}
-if(!empty($register_data['phoneNumber']))
-{
  $phoneNumber = $register_data['phoneNumber'];
-}
  $query = "INSERT INTO RUTravelUsers(FirstName, LastName, Email, UserPassword, Telephone, UserAddress, isAdmin)VALUES('".$firstName."', '".$lastName."', '".$email."', '".$password."', '".$phoneNumber."', '".$address."', '".$isAdmin."')";
  if ($conn->query($query) === TRUE){
   $registered = true;
--- server/places/reviews/insert.php
+++ server/places/reviews/insert.php
@@ -6,30 +6,12 @@ $review_data = json_decode(file_get_contents("php://input"), true);
 $message = '';
 $name =  $_SESSION["FirstName"]. " ".  $_SESSION["LastName"];
 $email =  $_SESSION["Email"];
-if(!empty($review_data['title']))
-{
  $title = $review_data['title'];
-}
-if(!empty($review_data['rating']))
-{
  $rating = $review_data['rating'];
-}
-if(!empty($review_data['description']))
-{
  $description = $review_data['description'];
-}
-if(!empty($review_data['date']))
-{
  $date = $review_data['date'];
-}
-if(!empty($review_data['attraction']))
-{
  $attraction = $review_data['attraction'];
-}
-if(!empty($review_data['newRating']))
-{
 $newRating = $review_data['newRating'];
-}
  $query = "INSERT INTO RUTravelReviews(ReviewerName, ReviewerEmail, ReviewDescription, ReviewTitle, ReviewDate, ReviewRating, Attraction)VALUES('".$name."', '".$email."', '".$description."', '".$title."', '".$date."', ".$rating.", '".$attraction."');";
  $conn->query($query);
  $query = "UPDATE RUTravelAttractions SET RatingTotal=".$newRating." WHERE Attraction='".$attraction."'";
